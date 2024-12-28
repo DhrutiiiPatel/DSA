aa377afeadeef7544504e322c5b9602edf9fbcd2
Dhruti <dhrutip678@gmail.com>
2024-12-28 22:44:20 +0530

    This is Linear Searching

---
+++ LinearSearch.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class LinearSearch{
+    public static int num(int[] array, int target) {
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == target) {
+                return i; 
+            }
+        }
+        return -1; 
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.print("Enter target element: ");
+        int target = scanner.nextInt();
+
+        int result = num(array, target);
+        if (result != -1) {
+            System.out.println("Target found at index: " + result);
+        } else {
+            System.out.println("Target not found in the array.");
+        }
+
+        scanner.close(); 
+    }
+}
