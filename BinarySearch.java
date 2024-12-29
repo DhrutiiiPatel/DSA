c815d2b7de5d4abcad1cb6e724e690423f52a534
Dhruti <dhrutip678@gmail.com>
2024-12-29 20:20:00 +0530

    This is Binary Searching.

---
+++ BinarySearch.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class BinarySearch {
+
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3,4,5};
+        System.out.println(BinarySearch(nums, 5));
+        
+    }
+
+    public static int BinarySearch(int[] num, int num_to_find) {
+        
+       int low = 0;
+       int high = num.length - 1;
+
+       while(low<=high){
+
+        int mid = (low+high)/2;
+        int mid_num = num[mid];
+
+        if(num_to_find == mid_num){
+            return mid;
+        }
+
+        if(num_to_find < mid){
+            high = mid - 1;
+        }
+
+        else{
+            low = mid + 1;
+        }
+
+       }
+       
+       return -1;
+     
+    }
+    
+}
